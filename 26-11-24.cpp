80d4abad28a41887fdacc9674426094ceb19f9b4
Akash Mahawar <142003846+AkashMahawar2002@users.noreply.github.com>
2024-11-26 22:30:52 +0530

    Add files via upload

---
+++ 26-11-24.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    // arr: input array
+    // Function to find maximum circular subarray sum.
+    int circularSubarraySum(vector<int> &arr) {
+
+        // your code here
+        int s=0;
+        int sum=0;
+        int res=0;
+        for(auto it:arr)
+        {
+            sum+=it;
+            s+=it;
+            if(s<0)
+            s=0;
+            
+            res=max(res,s);
+        }
+        s=0;
+        int x=INT_MAX;
+        for(auto it:arr)
+        {
+            s+=it;
+            if(s>0)
+            s=0;
+            
+            x=min(x,s);
+        }
+        res=max(res,sum-x);
+        return res;
+    
+    }
+};
