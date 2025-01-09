e8ab15e213c642b84fb97ebec9e1489c94eef8a9
Akash Mahawar <142003846+AkashMahawar2002@users.noreply.github.com>
2025-01-09 20:43:27 +0530

    Indexes of Subarray Sum

---
+++ 09-01-2025.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> subarraySum(vector<int> &arr, int target) {
+        // code here
+         unordered_map<int,int>mp;
+        int n=arr.size();
+        int sum=0;
+        mp[0]=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(mp.find(sum-target)!=mp.end()){
+                return {mp[sum-target]+1,i+1};
+            }
+            if(mp.find(sum)==mp.end())
+            mp[sum]=i+1;
+        }
+        return {-1};
+    }
+};
